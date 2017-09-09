25c01ab3af2a76a9c9be107a59c991c96b94df4c
Francis <sonsofix@yahoo.ca>
2017-09-08 17:29:14 -0700

    Finalized index

--- index.php
+++ index.php
@@ -11,7 +11,42 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-        echo 'Hello World';
+        include('Student.php');
+
+        $students = array();
+
+        $first = new Student();
+        $first->surname = "Doe";
+        $first->first_name = "John";
+        $first->add_email('home', 'john@doe.com');
+        $first->add_email('work', 'jdoe@mcdonalds.com');
+        $first->add_grade(65);
+        $first->add_grade(75);
+        $first->add_grade(55);
+        $students['j123'] = $first;
+
+        $second = new Student();
+        $second->surname = "Einstein";
+        $second->first_name = "Albert";
+        $second->add_email('home', 'albert@braniacs.com');
+        $second->add_email('work1', 'a_einstein@bcit.ca');
+        $second->add_email('work2', 'albert@physics.mit.edu');
+        $second->add_grade(95);
+        $second->add_grade(80);
+        $second->add_grade(50);
+        $students['a456'] = $second;
+
+        $me = new Student();
+        $me->surname = "Carreon";
+        $me->first_name = "Francis";
+        $me->add_email('school', 'fcarreon1@my.bcit.ca');
+        $me->add_grade(420);
+        $students['h3h'] = $me;
+
+        ksort($students); // one of the many sort functions
+
+        foreach ($students as $student)
+            echo $student->toString();
         ?>
     </body>
 </html>
